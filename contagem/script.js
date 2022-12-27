e5a79fde36f286e447a883d7ca6e2af2441709c2
Daniel Gaioso <soudanielgleidson@gmail.com>
2022-12-26 22:57:04 -0300

    aiai

--- contagem/script.js
+++ contagem/script.js
@@ -1,3 +1,22 @@
-function () {
+function count() {
+    var begin = document.getElementById('begin')
+    var end = document.getElementById('end')
+    var gap = document.getElementById('gap')
+    var result = document.getElementById('result')
 
+    if(begin.value.length == 0 || end.value.length == 0 || gap.value.length == 0) {
+        result.innerHTML = `Opa... Todos os campos devem ser preenchidos com um número diferente de zero`
+    }else if (gap.value.length <= 0) {
+        result.innerHTML = `Opa... o gap não pode ser igual ou menor a 0. Tente novamente.`
+    }else {
+        result.innerHTML = `Counting: `
+        var i = Number(begin.value);
+        var f = Number(end.value);
+        var p = Number(gap.value);
+
+        for(var c = i; c<=f; c+= p){
+            result.innerHTML += `${c} -->`
+        }
+    }
+    //result.innerHTML = `end`;
 }

ec6bd448506dcae39ee39aae1e97633d80496b96
Daniel Gaioso <soudanielgleidson@gmail.com>
2022-12-26 23:24:18 -0300

    +1

--- contagem/script.js
+++ contagem/script.js
@@ -1,22 +1,26 @@
 function count() {
-    var begin = document.getElementById('begin')
-    var end = document.getElementById('end')
-    var gap = document.getElementById('gap')
-    var result = document.getElementById('result')
+    let i = document.getElementById('begin')
+    let f = document.getElementById('end')
+    let p = document.getElementById('gap')
+    let result = document.getElementById('result')
 
-    if(begin.value.length == 0 || end.value.length == 0 || gap.value.length == 0) {
+    if(i.value.length == 0 || f.value.length == 0 || p.value.length == 0) {
         result.innerHTML = `Opa... Todos os campos devem ser preenchidos com um número diferente de zero`
-    }else if (gap.value.length <= 0) {
+    }else if (p.value <= 0) {
         result.innerHTML = `Opa... o gap não pode ser igual ou menor a 0. Tente novamente.`
     }else {
         result.innerHTML = `Counting: `
-        var i = Number(begin.value);
-        var f = Number(end.value);
-        var p = Number(gap.value);
-
-        for(var c = i; c<=f; c+= p){
-            result.innerHTML += `${c} -->`
+        if(Number(i.value) < Number(f.value)) {
+            //Contagem crescente
+            for (let c = Number(i.value); c <= Number(f.value); c += Number(p.value)) {
+                result.innerHTML += `${c} \u{1F449} `
+            }
+        } else {
+            //Contagem regressiva
+            for (let c = Number(i.value); c >= Number(f.value); c -= Number(p.value)) {
+                result.innerHTML += `${c} \u{1F449} `
+            }
+            result.innerHTML += `\u{1F3C1}`
         }
     }
-    //result.innerHTML = `end`;
 }

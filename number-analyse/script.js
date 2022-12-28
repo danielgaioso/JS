babcacde895983e73d0d4f383e398b2c0fb38ec7
Daniel Gaioso <soudanielgleidson@gmail.com>
2022-12-28 09:02:33 -0300

    terninado o ex

--- number-analyse/script.js
+++ number-analyse/script.js
@@ -22,9 +22,40 @@ function inList(num, list){
 
 function add() {
     if(isNumber(n.value) && !inList(n.value, v)){
-
+        v.push(Number(n.value))
+        let item = document.createElement('option')
+        item.text = `Número ${n.value} adicionado`
+        s.appendChild(item)
+        r.innerHTML = ``
     } else{
         window.alert('Valor invalido ou já encontrado na lista')
     }
-
+    n.value = ``
+    n.focus()
 }
+
+function analyze() {
+    if (s.lenght == 0) {
+    window.alert('nenhum valor encontrado')}
+    else {
+        let total = v.length
+        let menor = v[0]
+        let maior = v[0]
+        let soma = 0
+        let media = 0
+        for(let pos in v){
+            soma += v[pos]
+            media = soma / total
+            if (v[pos] > maior)
+                maior = v[pos]
+            if (v[pos] < menor){
+                menor = v[pos]
+        }
+
+        r.innerHTML = ``
+        r.innerHTML += `<p>${total} números foram informados.</p>`
+        r.innerHTML += `<p> O maior valor é ${maior} e o menor é o ${menor} </p>`
+        r.innerHTML += `<p>A soma de todos os números é ${soma}.</p>`
+        r.innerHTML += `<p>A média dos números cadastrados é de ${media. toFixed(3)}.</p>`
+    }
+}}

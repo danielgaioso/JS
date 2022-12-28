42a7db118dacf825204d40346e6fc5d8b4090287
Daniel Gaioso <soudanielgleidson@gmail.com>
2022-12-28 08:25:58 -0300

    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa

--- number-analyse/script.js
+++ number-analyse/script.js
@@ -1,7 +1,30 @@
-function add() {
+var n = document.getElementById('inumber')
+var s = document.getElementById('iselect')
+var r = document.getElementById('result')
+var v = []
+
+function isNumber(num){
+    if(Number(num) >= 1 && Number(num)  <= 100){
+        return true
+    }else{
+        return false
+    }
+}
+
+function inList(num, list){
+    if(list.indexOf(Number(num)) != -1){
+        return true
+    } else {
+        return false
+    }
 
 }
 
-function analyse{
-    
+function add() {
+    if(isNumber(n.value) && !inList(n.value, v)){
+
+    } else{
+        window.alert('Valor invalido ou já encontrado na lista')
+    }
+
 }
